eeea6881c9dec5ee880d7cecbb210eed4be0950c
Piotr S. Staszewski <p.staszewski@gmail.com>
2014-11-14 22:20:07 +0100

    Initial commit

---
+++ tvrage.go
@@ -0,0 +1,117 @@
+package tvrage
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+type Show struct {
+	ID             int      `xml:"showid"`
+	Name           string   `xml:"name"`
+	Link           string   `xml:"link"`
+	Country        string   `xml:"country"`
+	Started        int      `xml:"started"`
+	Ended          int      `xml:"ended"`
+	Seasons        int      `xml:"seasons"`
+	Status         string   `xml:"status"`
+	Classification string   `xml:"classification"`
+	Genres         []string `xml:"genres>genre"`
+}
+
+type tvrageTime struct {
+	time.Time
+}
+
+func (t *tvrageTime) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	var v string
+	d.DecodeElement(&v, &start)
+	parsed, err := time.Parse(TIMEFMT, v)
+	if err != nil {
+		return nil
+	}
+	*t = tvrageTime{parsed}
+	return nil
+}
+
+type Episode struct {
+	Season     int
+	Ordinal    int        `xml:"epnum"`
+	Number     int        `xml:"seasonnum"`
+	Production string     `xml:"prodnum"`
+	AirDate    tvrageTime `xml:"airdate"`
+	Link       string     `xml:"link"`
+	Title      string     `xml:"title"`
+}
+
+type resultSeason struct {
+	Number   int       `xml:"no,attr"`
+	Episodes []Episode `xml:"episode"`
+}
+
+type resultEpisodeList struct {
+	Total   int            `xml:"totalseasons"`
+	Seasons []resultSeason `xml:"Episodelist>Season"`
+}
+
+type resultSearch struct {
+	Shows []Show `xml:"show"`
+}
+
+const (
+	SEARCHURL = `http://services.tvrage.com/feeds/search.php?show=%s`      // URL for show searching
+	EPLISTURL = `http://services.tvrage.com/feeds/episode_list.php?sid=%d` // URL for episode list
+	TIMEFMT   = `2006-01-02`                                               // time.Parse format string for air date
+)
+
+var (
+	Client = &http.Client{} // default HTTP client
+)
+
+func parseSearchResult(in io.Reader) ([]Show, error) {
+	r := resultSearch{}
+	x := xml.NewDecoder(in)
+	if err := x.Decode(&r); err != nil {
+		return nil, err
+	}
+	return r.Shows, nil
+}
+
+func Search(name string) ([]Show, error) {
+	q := fmt.Sprintf(SEARCHURL, url.QueryEscape(name))
+	r, err := Client.Get(q)
+	if err != nil {
+		return nil, err
+	}
+	defer r.Body.Close()
+	return parseSearchResult(r.Body)
+}
+
+func parseEpisodeListResult(in io.Reader) ([]Episode, error) {
+	var es []Episode
+	r := resultEpisodeList{}
+	x := xml.NewDecoder(in)
+	if err := x.Decode(&r); err != nil {
+		return nil, err
+	}
+	for _, s := range r.Seasons {
+		for _, e := range s.Episodes {
+			e.Season = s.Number
+			es = append(es, e)
+		}
+	}
+	return es, nil
+}
+
+func EpisodeList(id int) ([]Episode, error) {
+	q := fmt.Sprintf(EPLISTURL, id)
+	r, err := Client.Get(q)
+	if err != nil {
+		return nil, err
+	}
+	defer r.Body.Close()
+	return parseEpisodeListResult(r.Body)
+}
---
+++ tvrage_test.go
@@ -0,0 +1,191 @@
+package tvrage
+
+import (
+	"strings"
+	"testing"
+	"time"
+)
+
+func TestParseSearchResult(t *testing.T) {
+	input := `<Results>
+<show>
+<showid>5410</showid>
+<name>Supernatural</name>
+<link>http://www.tvrage.com/Supernatural</link>
+<country>US</country>
+<started>2005</started>
+<ended>0</ended>
+<seasons>10</seasons>
+<status>Returning Series</status>
+<classification>Scripted</classification>
+<genres>
+<genre>Action</genre>
+<genre>Adventure</genre>
+<genre>Drama</genre>
+<genre>Horror/Supernatural</genre>
+<genre>Sci-Fi</genre>
+</genres>
+</show>
+<show>
+<showid>2032</showid>
+<name>Supernatural (1977)</name>
+<link>http://www.tvrage.com/supernatural-1977</link>
+<country>UK</country>
+<started>1977</started>
+<ended>1977</ended>
+<seasons>1</seasons>
+<status>Canceled/Ended</status>
+<classification>0</classification>
+<genres/>
+</show>
+<show>
+<showid>27870</showid>
+<name>Supernatural: The Animation</name>
+<link>http://www.tvrage.com/supernatural-the-animation</link>
+<country>JP</country>
+<started>2011</started>
+<ended>2011</ended>
+<seasons>1</seasons>
+<status>Canceled/Ended</status>
+<classification>Animation</classification>
+<genres>
+<genre>Anime</genre>
+<genre>Horror/Supernatural</genre>
+</genres>
+</show>
+</Results>`
+
+	res, err := parseSearchResult(strings.NewReader(input))
+	if err != nil {
+		t.Errorf("Decode error: %s", err)
+		t.FailNow()
+	}
+
+	if len(res) != 3 {
+		t.Errorf("Length mismatch: %d != %d", len(res), 3)
+	}
+	if res[0].ID != 5410 {
+		t.Errorf("(1) Show ID mismatch: %d != %d", res[0].ID, 5410)
+	}
+	if res[1].Name != "Supernatural (1977)" {
+		t.Errorf("(2) Show Name mismatch: %s != %s", res[1].Name, "Supernatural (1977)")
+	}
+	if len(res[2].Genres) != 2 {
+		t.Errorf("(3) Genres length mismatch: %d != %d", len(res[2].Genres), 2)
+	}
+
+	res, err = parseSearchResult(strings.NewReader(``))
+	if err == nil {
+		t.Errorf("Didn't fail with empty data")
+		t.FailNow()
+	}
+}
+
+func TestSearchLive(t *testing.T) {
+	if testing.Short() {
+		t.SkipNow()
+	}
+
+	r, err := Search("archer (2009)")
+	if err != nil {
+		t.Errorf("Search error: %s", err)
+		t.FailNow()
+	}
+
+	if len(r) < 1 {
+		t.Errorf("Less than one Show decoded")
+	}
+}
+
+func TestParseEpisodeListResult(t *testing.T) {
+	input := `<Show>
+<name>Supernatural</name>
+<totalseasons>10</totalseasons>
+<Episodelist>
+<Season no="1">
+<episode>
+<epnum>0</epnum>
+<seasonnum>00</seasonnum>
+<prodnum/>
+<airdate>0000-00-00</airdate>
+<link>
+http://www.tvrage.com/Supernatural/episodes/1065190732
+</link>
+<title>Unaired Pilot</title>
+</episode>
+<episode>
+<epnum>1</epnum>
+<seasonnum>01</seasonnum>
+<prodnum>475285</prodnum>
+<airdate>2005-09-13</airdate>
+<link>http://www.tvrage.com/Supernatural/episodes/166205</link>
+<title>Pilot</title>
+</episode>
+</Season>
+<Season no="2">
+<episode>
+<epnum>23</epnum>
+<seasonnum>01</seasonnum>
+<prodnum>3T5501</prodnum>
+<airdate>2006-09-28</airdate>
+<link>http://www.tvrage.com/Supernatural/episodes/386441</link>
+<title>In My Time of Dying</title>
+</episode>
+<episode>
+<epnum>24</epnum>
+<seasonnum>02</seasonnum>
+<prodnum>3T5502</prodnum>
+<airdate>2006-10-05</airdate>
+<link>http://www.tvrage.com/Supernatural/episodes/412873</link>
+<title>Everybody Loves a Clown</title>
+</episode>
+</Season>
+</Episodelist>
+</Show>`
+
+	res, err := parseEpisodeListResult(strings.NewReader(input))
+	if err != nil {
+		t.Errorf("Decode error: %s", err)
+		t.FailNow()
+	}
+
+	if len(res) != 4 {
+		t.Errorf("Length mismatch: %d != %d", len(res), 4)
+	}
+	if res[0].Title != "Unaired Pilot" {
+		t.Errorf("(1) Episode Title mismatch: %s != %s", res[0].Title, "Unaired Pilot")
+	}
+	if res[1].Season != 1 && res[1].Number != 1 {
+		t.Errorf("(2) Episode Season/Number mismatch: S%dE%d != %s", res[1].Season, res[1].Number, "S1E1")
+	}
+	testDate, err := time.Parse(TIMEFMT, "2006-09-28")
+	if err != nil {
+		t.Errorf("Couldn't make testDate: %s", err)
+	} else {
+		if !res[2].AirDate.Equal(testDate) {
+			t.Errorf("(3) AirDate mismatch: %s != %s", res[2].AirDate, testDate)
+		}
+	}
+
+	res, err = parseEpisodeListResult(strings.NewReader(``))
+	if err == nil {
+		t.Errorf("Didn't fail with empty data")
+		t.FailNow()
+	}
+}
+
+func TestEpisodeListLive(t *testing.T) {
+	if testing.Short() {
+		t.SkipNow()
+	}
+
+	r, err := EpisodeList(5410)
+	if err != nil {
+		t.Errorf("EpisodeList error: %s", err)
+		t.FailNow()
+	}
+
+	if len(r) < 1 {
+		t.Errorf("Less than one Episode decoded")
+	}
+}
