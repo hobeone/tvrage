7be08692095b32db65684a36c29b7ebdaac5ed23
Piotr S. Staszewski <p.staszewski@gmail.com>
2014-11-15 11:54:04 +0100

    Add basic docs

--- tvrage.go
+++ tvrage.go
@@ -1,3 +1,5 @@
+// Package tvrage provides basic access to tvrage.com services for finding out the last
+// and next episodes of a given TV show (plus a bit more), no API key required.
 package tvrage
 
 import (
@@ -9,6 +11,7 @@ import (
 	"time"
 )
 
+// Show maps all available show data, as retrieved via Search.
 type Show struct {
 	ID             int      `xml:"showid"`
 	Name           string   `xml:"name"`
@@ -22,14 +25,17 @@ type Show struct {
 	Genres         []string `xml:"genres>genre"`
 }
 
+// String returns a pretty string for a given Show.
 func (s Show) String() string {
 	return fmt.Sprintf("%s [%d - %s]", s.Name, s.Started, s.Status)
 }
 
+// tvrageTime is a thin shim over time.Time used to implement XML unmarshaling.
 type tvrageTime struct {
 	time.Time
 }
 
+// UnmarshalXML implements time.Time XML unmarshaling for tvrage.com air date format.
 func (t *tvrageTime) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
 	var v string
 	d.DecodeElement(&v, &start)
@@ -41,6 +47,7 @@ func (t *tvrageTime) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error 
 	return nil
 }
 
+// Episode maps all available episode data, as retrieved via EpisodeList.
 type Episode struct {
 	Season     int
 	Ordinal    int        `xml:"epnum"`
@@ -51,10 +58,13 @@ type Episode struct {
 	Title      string     `xml:"title"`
 }
 
+// String returns a pretty string for a given Episode.
 func (e Episode) String() string {
 	return fmt.Sprintf(`S%02dE%02d "%s"`, e.Season, e.Number, e.Title)
 }
 
+// DeltaDays returns a pretty string indicating the delta in days between now
+// and the episode air date.
 func (e *Episode) DeltaDays() string {
 	d := int(e.AirDate.Sub(time.Now()).Hours() / 24.0)
 	if d < 0 {
@@ -74,8 +84,11 @@ func (e *Episode) DeltaDays() string {
 	}
 }
 
+// Episodes is a thin shim over []Episodes to enable methods on Episode slices.
 type Episodes []Episode
 
+// Last returns the last aired episode from the given slice of Episodes and true
+// if it was possible to find such episode.
 func (es Episodes) Last() (Episode, bool) {
 	var r Episode
 	t := time.Now()
@@ -94,6 +107,8 @@ func (es Episodes) Last() (Episode, bool) {
 	}
 }
 
+// Next returns the next episode to air from the given slice of Episodes and true
+// if it was possible to find such episode.
 func (es Episodes) Next() (Episode, bool) {
 	var r Episode
 	t := time.Now()
@@ -108,16 +123,19 @@ func (es Episodes) Next() (Episode, bool) {
 	return r, false
 }
 
+// resultSeason is an internal intermediate struct used for processing EpisodeList results.
 type resultSeason struct {
 	Number   int       `xml:"no,attr"`
 	Episodes []Episode `xml:"episode"`
 }
 
+// resultEpisodeList is an internal final struct used for processing EpisodeList results.
 type resultEpisodeList struct {
 	Total   int            `xml:"totalseasons"`
 	Seasons []resultSeason `xml:"Episodelist>Season"`
 }
 
+// resultSearch is an internal final struct used for processing Search results.
 type resultSearch struct {
 	Shows []Show `xml:"show"`
 }
@@ -133,6 +151,7 @@ var (
 	Client = &http.Client{} // default HTTP client
 )
 
+// parseSearchResult parses the XML as retrieved by Search.
 func parseSearchResult(in io.Reader) ([]Show, error) {
 	r := resultSearch{}
 	x := xml.NewDecoder(in)
@@ -142,6 +161,7 @@ func parseSearchResult(in io.Reader) ([]Show, error) {
 	return r.Shows, nil
 }
 
+// Search retrieves matched shows for the given name.
 func Search(name string) ([]Show, error) {
 	q := fmt.Sprintf(SEARCHURL, url.QueryEscape(name))
 	r, err := Client.Get(q)
@@ -152,6 +172,8 @@ func Search(name string) ([]Show, error) {
 	return parseSearchResult(r.Body)
 }
 
+// parseEpisodeListResults parses the XML as retrieved by EpisodeList.
+// It fills in the season number and returns a slice of Episodes.
 func parseEpisodeListResult(in io.Reader) (Episodes, error) {
 	var es Episodes
 	r := resultEpisodeList{}
@@ -168,6 +190,7 @@ func parseEpisodeListResult(in io.Reader) (Episodes, error) {
 	return es, nil
 }
 
+// EpisodeList retrieves the list of episodes for the given show id.
 func EpisodeList(id int) (Episodes, error) {
 	q := fmt.Sprintf(EPLISTURL, id)
 	r, err := Client.Get(q)
